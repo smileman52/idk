b3f12b25be71e5f9fd534229908c74436fc2b507
EpicLifeDog <105741808+EpicLifeDog@users.noreply.github.com>
2022-05-27 09:36:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,30 +9,4 @@ let Letter3 = 0;
 let Letter4 = 0;
 let Letter5 = 0;
 let Letter6 = 0;
-while (Guess.includes(Password)) {
-  Letter6 += 1;
-  if (Letter6 > 36) {
-    Letter5 += 1;
-    Letter6 = 1;
-  }
-  if (Letter5 > 36) {
-    Letter4 += 1;
-    Letter5 = 1;
-  }
-  if (Letter4 > 36) {
-    Letter3 += 1;
-    Letter4 = 1;
-  }
-  if (Letter3 > 36) {
-    Letter2 += 1;
-    Letter3 = 1;
-  }
-  if (Letter2 > 36) {
-    Letter1 += 1;
-    Letter2 = 1;
-  } 
-  Guess = Digits.join(Letter1 + Letter2 + Letter3 + Letter4 + Letter5 + Letter6);
-  GuessesMade += 1;
-  MaximumGuesses -= 1;
-}
 document.getElementById("done").innerHTML = Done! Your Password is " + Guess + ". This took " + GuessesMade + " tries.", 0, 0;
