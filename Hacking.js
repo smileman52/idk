b6a3b6291b0628ad361918c3f65e7724175f6deb
smile52 <104168698+smileman52@users.noreply.github.com>
2022-05-26 17:11:43 -0700

    Update Hacking.js

--- Hacking.js
+++ Hacking.js
@@ -1,5 +1,6 @@
 var Digits = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var Guess = 0;
+var Password = 0;
 var GuessesMade = 0;
 var MaximumGuesses = 2176782336;
 var Letter1 = 0;
@@ -8,9 +9,9 @@ var Letter3 = 0;
 var Letter4 = 0;
 var Letter5 = 0;
 var Letter6 = 0;
-var Password = window.prompt("Enter the password you want to hack: "); 
+var TempPassword = window.prompt("Enter the password you want to hack: "); 
 background(255, 255, 255);
-alert("The password is " + Password);
+text(Password,0,50);
 background(255, 255, 255);
 text("Cracking...", 0, 0);
 while (Guess.includes(Password)) {
@@ -38,6 +39,7 @@ while (Guess.includes(Password)) {
   Guess = Digits.join(Letter1 and Letter2 and Letter3 and Letter4 and Letter5 and Letter6);
   GuessesMade += 1;
   MaximumGuesses -= 1;
+  text(Guess, 0, 80)
 }
 background(255, 255, 255);
 text("Done! Your Password is " + Guess + ". This took " + GuessesMade + " tries.", 0, 0);
