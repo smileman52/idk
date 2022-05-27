b7138f0ba656a24a31442535be98feb6d452e550
smile52 <104168698+smileman52@users.noreply.github.com>
2022-05-26 20:24:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var Letter3 = 0;
 var Letter4 = 0;
 var Letter5 = 0;
 var Letter6 = 0;
-var name = window.prompt("Enter the name you want to hack: "); 
-alert("The password is " + name); 
+var password = window.prompt("Enter the password you want to hack: "); 
+alert("The password is " + password); 
 text("Cracking...", 0, 0);
 while (Guess.includes(Password)) {
   Letter6 += 1;
