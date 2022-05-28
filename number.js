f51252c38077124f166c908105d32d790ea18465
smile52 <104168698+smileman52@users.noreply.github.com>
2022-05-28 12:27:11 -0700

    Create number.js

---
+++ number.js
@@ -0,0 +1,12 @@
+var Password = window.prompt("Enter the password you want to hack(numbers only): ")
+        let counts=setInterval(updated);
+        let upto=0;
+        function updated(){
+            var count= document.getElementById("counter");
+            count.innerHTML=++upto
+          if(upto=Password)
+          {
+          alert("The password is" + upto)
+              window.stop()
+          }
+          }
