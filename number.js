f15b34c7b75ad80c4136afa3fbc19e3f615860e7
smile52 <104168698+smileman52@users.noreply.github.com>
2022-06-01 11:36:38 -0700

    Update number.js

--- number.js
+++ number.js
@@ -1,15 +1,14 @@
 var Password = window.prompt("Enter the password you want to hack(numbers only): ")
 let counts=setInterval(updated);
 let upto=0;
-function alert(){
-alert("I cracked your Password! "+ upto)
-}
 function updated(){
         var count= document.getElementById("counter");
-        count.innerHTML=++upt0
+        count.innerHTML=++upto
         if(upto==Password)
                 {
                  document.getElementById("counter").innerHTML = upto;
-                 window.setTimeout(alert, 0);
+                 window.stop()
+                 alert("I cracked your Password! "+ upto)
+                 window.stop()
                 }
 }
